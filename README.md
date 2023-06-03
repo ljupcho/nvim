a7a33ff70c1fdbb17ac8c2f4c804ba0707fd52c8
Ljupcho <lui10e6@gmail.com>
2023-06-03 11:55:14 +0200

    configure lspsage with right keys

---
+++ README.md
@@ -0,0 +1,7 @@
+- you need to have `node` and `npm` installed
+- you need to have `rd` and `fd` installed for telescope with fz search
+- make sure all is green for:
+  :checkhealth telescope
+  :checkhealth lsp
+  :checkhealth mason
+- configure the lsp popup definitions under `plugins/lsp/lspsage.lua` and `pugins/lsp/lspconfig.lua` for right keys
--- lua/ljupcho/plugins/lsp/lspsaga.lua
+++ lua/ljupcho/plugins/lsp/lspsaga.lua
@@ -15,4 +15,34 @@ saga.setup({
   definition_action_keys = {
     edit = "<CR>",
   },
+  preview = {
+    lines_above = 0,
+    lines_below = 10,
+  },
+  finder = {
+    max_height = 0.5,
+    min_width = 30,
+    force_max_height = false,
+    keys = {
+      jump_to = "p",
+      -- expand_or_jump = "o",
+      expand_or_jump = "<CR>",
+      vsplit = "s",
+      split = "i",
+      tabe = "t",
+      tabnew = "r",
+      quit = { "q", "<ESC>" },
+      close_in_preview = "<ESC>",
+    },
+  },
+  code_action = {
+    num_shortcut = true,
+    show_server_name = false,
+    extend_gitsigns = true,
+    keys = {
+      -- string | table type
+      quit = "q",
+      exec = "<CR>",
+    },
+  },
 })
